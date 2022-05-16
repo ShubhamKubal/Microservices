f95250d2971351fe47a270107ada9d928a9e62dd
ShubhamKubal <skubal2015@gmail.com>
2022-05-16 19:37:05 +0530

    Implemented basic Spring Cloud Gateway In Microservices

---
+++ SpringCloudFeignBookService/src/main/java/com/microservices/training/SpringCloudFeignBookServiceApplication.java
@@ -0,0 +1,15 @@
+package com.microservices.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringCloudFeignBookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudFeignBookServiceApplication.class, args);
+	}
+
+}
--- SpringCloudPaymentService/src/main/java/com/microservices/training/controller/PaymentRestController.java
+++ SpringCloudPaymentService/src/main/java/com/microservices/training/controller/PaymentRestController.java
@@ -1,6 +1,8 @@
 package com.microservices.training.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +16,17 @@ public class PaymentRestController {
 	@Autowired
 	private CartRestConsumer cartConsumer;
 	
+	@Value("${server.port}")
+	private String port;
+	
 	@GetMapping("/data")
 	public String getPaymentDate() {
 		return "FROM PAYMENT-SERVICE : " + cartConsumer.getCartInfo();
 	}
+	
+	@GetMapping("/info")
+    public ResponseEntity<String> showPaymentInfo() {
+       return ResponseEntity.ok("FROM PAYMENT SERVICE, Port# is: " + port);
+    }
 
 }
